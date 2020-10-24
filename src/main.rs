df51f740d393c00ffe097afcfd4a112b85a77ccb
otsukaresamaja <christophersugai@gmail.com>
2020-10-23 23:45:33 -1000

    fasta

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+
+// // -------------------------------CLI Parameters-------------------------------
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+
+    /// Path to the file to read
+    #[structopt(
+        short="i", 
+        long = "--infile",
+        parse(from_os_str)
+    )]
+        infile: std::path::PathBuf,
+}
+// // -------------------------------Stop CLI Parameters-------------------------------
+
+// // -------------------------------BAM file-------------------------------
+// extern crate bam;
+// use std::io;
+// use bam::RecordWriter;
+
+// fn main() {
+//     let args = Cli::from_args();
+//     let filestr = std::fs::read_to_string(&args.infile)
+//     .expect("could not read file");
+
+//     // Do Stuff
+//     // Documentation Example below: https://docs.rs/bam/0.1.1/bam/
+//     let mut reader = bam::IndexedReader::from_path(filestr).unwrap();
+//     let output = io::BufWriter::new(io::stdout());
+//     let mut writer = bam::BamWriter::build()
+//         .write_header(false)
+//         .from_stream(output, reader.header().clone()).unwrap();
+//     for record in reader.fetch(&bam::Region::new(2, 600_000, 700_000)).unwrap() {
+//         let record = record.unwrap();
+//         writer.write(&record).unwrap();
+//     }
+// }
+// // -------------------------------Stop BAM file-------------------------------
+
+// // -------------------------------SAM file-------------------------------
+// extern crate bam;
+
+// use std::io;
+// use bam::sam::SamReader;
+ 
+// fn main() {
+//     let args = Cli::from_args();
+//     let filestr = std::fs::read_to_string(&args.infile)
+//     .expect("could not read file");
+
+//     // Do something.
+//     // Documentation Example: https://docs.rs/bam/0.1.1/bam/sam/struct.SamReader.html
+//     let reader = SamReader::from_path("in.sam").unwrap();
+//     let mut reader = bam::SamReader::from_path("in.sam").unwrap();
+//     for record in reader {
+//         let record = record.unwrap();
+//     }
+// }
+
+// // -------------------------------Stop SAM file-------------------------------
+
+// // -------------------------------FASTQ file-------------------------------
+// extern crate seq_io;
+
+// use seq_io::fastq::{Reader,Record};
+
+// fn main() {
+//     let args = Cli::from_args();
+//     let filestr = std::fs::read_to_string(&args.infile)
+//     .expect("could not read file");
+
+//     // Do something.
+//     // Documentation Example: https://docs.rs/seq_io/0.3.1/seq_io/
+//     let mut reader = Reader::from_path(filestr).unwrap();
+//     while let Some(record) = reader.next() {
+//         let record = record.expect("Error reading record");
+//         println!("{}", record.id().unwrap());
+//     }
+// }
+
+// // -------------------------------Stop FASTQ file-------------------------------
+
+// // -------------------------------FASTA file-------------------------------
+// extern crate seq_io;
+
+// use seq_io::fasta::{Reader,Record};
+
+// fn main() {
+//     let args = Cli::from_args();
+//     let filestr = std::fs::read_to_string(&args.infile)
+//     .expect("could not read file");
+
+//     // Do something.
+//     // Documentation Example: https://docs.rs/seq_io/0.3.1/seq_io/
+//     let mut reader = Reader::from_path(filestr).unwrap();
+//     while let Some(record) = reader.next() {
+//         let record = record.expect("Error reading record");
+//         println!("{}", record.id().unwrap());
+//     }
+// }
+// // -------------------------------Stop FASTA file-------------------------------
+
